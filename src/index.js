4173ea5d6873973787809aef0ae8686194f5fd3f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-28 01:01:23 +0530

    Provide store to child components via provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App';
+import bookupdateReducer from './Reducers/books';
+
+const store = createStore(bookupdateReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
