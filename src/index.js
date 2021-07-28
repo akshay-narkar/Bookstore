4737c997576685fd0191f3c584418e45603a8722
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-29 02:32:54 +0530

    Use a preloaded state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/Presentational/App';
-import rootReducer from './Reducers/index';
+import App from './Components/App';
+// import rootReducer from './Reducers/index';
+import rootReducer from './Reducers/books';
 
-const store = createStore(rootReducer);
+const initstate = {
+  books: [{
+    Id: Math.random(),
+    Title: 'Harry Potter & The Prisoners of Askaban',
+    Category: 'Kids',
+  },
+  { Id: Math.random(), Title: 'Shoe Dog', Category: 'Biography' },
+  { Id: Math.random(), Title: 'The Great Gatsby', Category: 'Action' }],
+};
+
+const store = createStore(rootReducer, initstate);
 
 ReactDOM.render(
   <Provider store={store}>
