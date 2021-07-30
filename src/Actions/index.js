a85a0fad86376febdb31583ed0e6ec860ee3e4d7
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-30 20:44:35 +0530

    Update Removebook action creator

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,14 +1,15 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const createBook = (book) => ({
-  type: CREATE_BOOK,
-  book,
-});
-
-const removeBook = (book) => ({
+function createBook(book) {
+  return ({
+    type: CREATE_BOOK,
+    book,
+  });
+}
+const removeBook = (index) => ({
   type: REMOVE_BOOK,
-  book,
+  index,
 });
 
 export {
