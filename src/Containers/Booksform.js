b541b40891a459662e3bd8749b15d21ddc79bf76
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-30 20:45:08 +0530

    Add Handlechange and submitchange to bookform

--- src/Containers/Booksform.js
+++ src/Containers/Booksform.js
@@ -1,116 +1,73 @@
-/*eslint-disable*/
-// import React, { useState } from 'react';
-// export default function Booksform() {
-//   const [state, setState1] = useState({
-//     title: null,
-//     category: null,
-//   });
-
-//   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-
-//   const handleChange = (event) => {
-//     if (event.target.id === 'exampleselectInput1') {
-//       setState1({
-//         category: event.target.value,
-//       });
-//     } else {
-//       setState1({
-//         title: event.target.value,
-//       });
-//     }
-//     console.log(state);
-//   };
-
-//   // const submitChange = () => {
-//   //   // addnewbook(state);
-//   //   console.log(addnewbook);
-//   //   console.log(state.title);
-//   //   console.log(state.category);
-//   // };
-
-//   return (
-//     <div className="container border mt-5 p-5">
-//       <div className="mb-3">
-//         <label htmlFor="exampleFormControlInput1" className="form-label">
-//           Book Title
-//           <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" onChange={handleChange} />
-//         </label>
-
-//       </div>
-//       <div className="mb-3">
-//         <label htmlFor="exampleselectInput1" className="form-label">
-
-//           Select Category
-
-//           <select className="form-select form-control" id="exampleselectInput1" aria-label="selectbooks" >
-//             {categories.map((x) => <option value={x} key={Math.random()}>{x}</option>)}
-//           </select>
-//         </label>
-//       </div>
-//       <button type="button" className="btn btn-dark ">Submit</button>
-//     </div>
-//   );
-// }
-
+// /*eslint-disable*/
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createBook } from '../Actions/index';
 
 function Booksform(props) {
-  const categories = ['Please select','Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const { addnewbook } = props;
   const [state, setState1] = useState({
-    title: null,
-    category: '',
+    Id: Math.floor((Math.random() * 1000000)),
+    Title: null,
+    Category: '',
   });
   const handleChange = (event) => {
-    if (event.target.id === 'exampleselectInput1' && event.target.value !== 'Please select') {
-      console.log(event.target);
-      setState1((prevstate) => ({ ...prevstate, category: event.target.value }));
+    if (event.target.id === 'exampleselectInput1' && event.target.value !== '') {
+      setState1((prevstate) => ({ ...prevstate, Category: event.target.value }));
     } else {
-      setState1((prevstate) => ({ ...prevstate, title: event.target.value }));
+      setState1((prevstate) => ({ ...prevstate, Title: event.target.value }));
     }
   };
 
-  const submitChange = () => {
+  const submitChange = (e) => {
+    e.preventDefault();
     addnewbook(state);
-    console.log(addnewbook);
-    console.log(state.title);
-    console.log(state.category);
+    setState1(() => ({ Title: null, Category: '' }));
   };
   return (
     <div className="container border mt-5 p-5">
-      {console.log(state)}
 
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Book Title
-          <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" onChange={handleChange} required />
-        </label>
+      <form onSubmit={submitChange}>
+        <div className="form-group">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Book Title
+              <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" onChange={handleChange} required />
+            </label>
 
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleselectInput1" className="form-label">
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleselectInput1" className="form-label">
 
-          Select Category
-          {/* ={[{ value, label }]} */}
-          {/* <select value={this.state.fruit} onChange={this.handleChange}>
+              Select Category
 
-            {options.map((option) => (
+              <select className="form-select form-control" value={state.Category} id="exampleselectInput1" aria-label="selectbooks" onChange={handleChange} required>
+                <option value="" disabled>Select category...</option>
 
-              <option value={option.value}>{option.label}</option>
-
-            ))} */}
-          <select className="form-select form-control" value = {state.category} id="exampleselectInput1" aria-label="selectbooks" onChange={handleChange} required>
-            {categories.map((x) => <option value={x} key={Math.random()}>{x}</option>)}
-          </select>
-        </label>
-      </div>
-      <button type="submit" className="btn btn-dark" onClick={submitChange}>Submit</button>
+                { /* eslint-disable-next-line */ }
+                {categories.map((x) => <option value={x} key={Math.random()}>{x}</option>)}
+              </select>
+            </label>
+          </div>
+          <button type="submit" className="btn btn-dark">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
+
+Booksform.propTypes = {
+  addnewbook: PropTypes.func.isRequired,
+};
+function mapDispatchToProps(dispatch) {
+  return {
+    addnewbook: (book) => {
+      dispatch(createBook(book));
+    },
+  };
+}
 /* eslint-disable-next-line */
-const mapDispatchToProps = (dispatch) => ({ addnewbook: (book) => { dispatch(createBook(book)); } });
+// const mapDispatchToProps = (dispatch) => ({ addnewbook: (book) => { dispatch(createBook(book)); } });
 
 export default connect(null, mapDispatchToProps)(Booksform);
--- src/Containers/Bookslist.js
+++ src/Containers/Bookslist.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../Components/book';
-
-const handleRemoveBook = (event) => {
-  console.log(event.target.parentElement.parentElement.firstElementChild.textContent);
-};
+import { removeBook } from '../Actions/index';
 
 function Bookslist(props) {
   const { booklist } = props;
+  const { removebook } = props;
+  const handleRemoveBook = (event) => {
+    removebook(event.target.parentElement.parentElement.id);
+  };
+
   return (
     <div className="mt-5">
       <h1>Books</h1>
@@ -23,8 +25,8 @@ function Bookslist(props) {
         </thead>
         <tbody>
 
-          {booklist.map((x) => (
-            <tr key={x.Id}>
+          {booklist.map((x, i) => (
+            <tr key={x.Id} id={i}>
               <Book
                 book={x}
                 clickHandler={handleRemoveBook}
@@ -43,8 +45,13 @@ function mapStateToProps(state) {
   return ({ booklist: books });
 }
 
+/* eslint-disable-next-line */
+const mapDispatchToProps = (dispatch) => ({ removebook: (book) => { dispatch(removeBook(book)); } });
+
 Bookslist.propTypes = {
   booklist: PropTypes.arrayOf(PropTypes.object).isRequired,
+  removebook: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(Bookslist);
+export default connect(mapStateToProps, mapDispatchToProps)(Bookslist);
+// export default Bookslist;
