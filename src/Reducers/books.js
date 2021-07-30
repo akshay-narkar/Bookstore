0ac6664a205f00bc243ae64cb0158297c63cd552
akshay-narkar <akshaynarkar1295@gmail.com>
2021-07-30 20:45:43 +0530

    Update reducer case Remove book

--- src/Reducers/books.js
+++ src/Reducers/books.js
@@ -1,14 +1,14 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../Actions';
 
 const bookupdateReducer = (state = [], action) => {
-  let statenew;
+  const statenew = { ...state };
   switch (action.type) {
     case CREATE_BOOK:
-      statenew = state.push(action.book);
+      statenew.books = [...statenew.books, action.book];
       return statenew;
     case REMOVE_BOOK:
-      statenew = [...state.slice(0, action.index),
-        ...state.slice(action.index + 1, state.length)];
+      statenew.books = [...statenew.books.slice(0, action.index),
+        ...statenew.books.slice(action.index + 1, state.length)];
       return statenew;
     default:
       return state;
