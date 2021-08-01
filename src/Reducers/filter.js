9916a4330d4e5ceebee93c665ee3cbf42cd3f2bc
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-02 02:47:28 +0530

    Create filter reducer function to change filter option

--- src/Reducers/filter.js
+++ src/Reducers/filter.js
@@ -1,15 +1,11 @@
-// import { CHANGE_FILTER } from '../Actions';
+import { CHANGE_FILTER } from '../Actions';
 
-const filterReducer = (state = null, action) => {
-  const statenew = state;
-  switch (action.category) {
-    case 'All':
-    //   statenew.books = [...statenew.books, action.book];
+const filterReducer = (state = [], action) => {
+  let statenew = { ...state };
+  switch (action.type) {
+    case CHANGE_FILTER:
+      statenew = { ...statenew, filter: action.category };
       return statenew;
-    // case REMOVE_BOOK:
-    //   statenew.books = [...statenew.books.slice(0, action.index),
-    //     ...statenew.books.slice(action.index + 1, state.length)];
-    //   return statenew;
     default:
       return state;
   }
