91cb824eb947cd4cd670a92392ad91f6609e0704
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-02 02:47:49 +0530

    Combine reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import filterReducer from './filter';
 import bookupdateReducer from './books';
 
-const rootReducer = combineReducers({ bookupdateReducer });
+const rootReducer = combineReducers({ bookupdateReducer, filterReducer });
 
 export default rootReducer;
