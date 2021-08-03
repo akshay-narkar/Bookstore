7f2f0396f5b0353c55246cc6b34986bf8a5e6c56
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-04 04:53:30 +0530

    Redesign the booklist

--- src/Components/book.js
+++ src/Components/book.js
@@ -1,22 +1,53 @@
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Book = (props) => {
   const { book, clickHandler } = props;
-  const { Id, Title, Category } = book;
-
+  const { Title, Category } = book;
   return (
-    <>
-      <td>{Id}</td>
-      <td>{Title}</td>
-      <td>{Category}</td>
-      <td><button type="button" className="btn btn-dark" onClick={clickHandler}>Remove</button></td>
-    </>
+    <div className="container">
+      <div className="row p-4 bookclass justify-content-between align-items-center">
+
+        {/* <td>{Id}</td> */}
+        <div className="left-mid col-5">
+          <p className="mb-1 categoryname monoreg">{Category}</p>
+          <h5 className="titlename roboto fw-bold">{Title}</h5>
+          <p className="mb-3 authorname monoreg">Anon Name</p>
+          <div>
+            <button type="button" className="btn ps-0 roboto authorname">Comments</button>
+            <span className="roboto authorname me-3"> | </span>
+            <button type="button" className="btn ps-0 roboto authorname" onClick={clickHandler}>Remove</button>
+            <span className="roboto authorname me-3"> | </span>
+            <button type="button" className="btn ps-0 roboto authorname">Edit</button>
+          </div>
+        </div>
+        <div className="center-mid  d-flex align-items-center col-3">
+          <div className="circularbar">
+            <CircularProgressbar value={Math.floor(Math.random() * 100)} />
+          </div>
+          <div className="p-3">
+            <h3 className=" monoreg">{`${Math.floor(Math.random() * 100)}%`}</h3>
+            <p className="mb-1 categoryname monoreg fw-normal opacity">Completed</p>
+
+          </div>
+        </div>
+
+        <div className="right-mid col-3">
+          <p className="mb-1 categoryname monoreg fw-normal opacity">CURRENT CHAPTER</p>
+          <p className="roboto chaptername">Chapter 15</p>
+          <div>
+            <button type="button" className="btn btn-primary roboto progressbtn mt-3">UPDATE PROGRESS</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.shape({
-    Id: PropTypes.number.isRequired,
+    // Id: PropTypes.number.isRequired,
     Title: PropTypes.string.isRequired,
     Category: PropTypes.string.isRequired,
   }).isRequired,
